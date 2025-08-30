7682ab12edbf9107c227b67c4bd9fcf0b0e69352
VrYt76 <valera121710@gmail.com>
2025-08-30 15:54:21 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,32 @@
-const CACHE_NAME = 'russian-tg-v1';
+const CACHE_NAME = 'russigram-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/Russian-Telegram/',
+  '/Russian-Telegram/index.html',
+  '/Russian-Telegram/site.webmanifest',
+  '/Russian-Telegram/favicon.ico',
+  '/Russian-Telegram/favicon-96x96.png',
+  '/Russian-Telegram/favicon-192x192.png',
+  '/Russian-Telegram/favicon-512x512.png',
+  '/Russian-Telegram/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
